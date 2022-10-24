72fa0b17b142d5f201e91f48121199963d500d20
jjav <razornice@gmail.com>
2022-10-24 11:07:58 -0300

    game module: compares and returns a proper feedback. still don't know if module or class

--- colors_module.rb
+++ colors_module.rb
@@ -1,11 +1,11 @@
-## 
+##
 # This module provides tools to geneate a combination of up to 4 colors.
 
 module ColorsTools
   # Valid colors are yellow, green, blue, prurple and orange
   COLORS = %w[yellow green blue purple red orange]
   # #manual_combination: Allows to select a combinantion of four intergers
-  # from 1 to 6. eache one representing a color. 
+  # from 1 to 6. eache one representing a color.
   #returns an array of those intergers
   def self.manual_combination
     puts "\nPick four colors\n
--- game.rb
+++ game.rb
@@ -1,7 +1,53 @@
+require "./colors_module"
 
-class Game
-  def initialize
-    @secret_line = Array.new
-    @table = Array.new
+include ColorsTools
+
+
+
+  def comp_codes(a, b)
+    a.intersection(b)
   end
-end
+
+  def comparator(a, b)
+    feedback_zero if a.intersection(b).length == 0
+
+    if a == b
+      same_code_feedback
+    else
+      a.intersection(b).each do |i|
+        unless a.index(i) == b.index(i)
+          relative_fedback
+        else
+          absulute_feedback
+        end
+      end
+    end
+  end
+
+  def feedback_zero
+    puts "Zero coincidences"
+  end
+
+  def relative_fedback
+    puts "Color missplaced"
+  end
+
+  def absulute_feedback
+    puts "Color properly placed"
+  end
+
+  def same_code_feedback
+    puts "Congratulations! You broke the secret code"
+  end
+
+# x = ColorsTools.random_color
+# puts x
+#
+# y = ColorsTools.random_color
+# puts y
+#
+# p comp_codes(x,y)
+
+x = [1,2,3,4]
+y = [1,2,3,5]
+comparator(x,y)

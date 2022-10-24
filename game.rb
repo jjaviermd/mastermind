77f87118c8eb46bedbedda932a084aeb88410c93
jjav <razornice@gmail.com>
2022-10-24 14:00:06 -0300

    gameTools module: branch

--- game.rb
+++ game.rb
@@ -1,14 +1,10 @@
-require "./colors_module"
 
-include ColorsTools
+module GameTools
 
+  attemps
 
-
-  def comp_codes(a, b)
-    a.intersection(b)
-  end
-
-  def comparator(a, b)
+  def compare (a, b)
+    attemps -= 1
     feedback_zero if a.intersection(b).length == 0
 
     if a == b
@@ -18,9 +14,16 @@ include ColorsTools
         unless a.index(i) == b.index(i)
           relative_fedback
         else
-          absulute_feedback
+          absolute_feedback
         end
       end
+    end
+  end
+
+
+  def game_start
+    if current_secret
+      puts "Secret code live./nTurns left:#{ATTEMPS}"
     end
   end
 
@@ -32,7 +35,7 @@ include ColorsTools
     puts "Color missplaced"
   end
 
-  def absulute_feedback
+  def absolute_feedback
     puts "Color properly placed"
   end
 
@@ -40,14 +43,9 @@ include ColorsTools
     puts "Congratulations! You broke the secret code"
   end
 
-# x = ColorsTools.random_color
-# puts x
-#
-# y = ColorsTools.random_color
-# puts y
-#
-# p comp_codes(x,y)
+  def turns
+    puts "#{ATTEMPS} attemps left" if ATTEMPS > 0
+    puts "Game over!" if ATTEMPS == 0
+  end
 
-x = [1,2,3,4]
-y = [1,2,3,5]
-comparator(x,y)
+end
---
+++ main.rb
@@ -0,0 +1,1 @@
+%w[./pc_player ./human_player ./game ./colors_module].each { |file| require file }
